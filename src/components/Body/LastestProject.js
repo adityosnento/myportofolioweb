95ec3cc9e78379c0c7e1ea533da6d43df26b474b
Adityo Nento <adityosnento11@gmail.com>
2020-05-04 19:36:03 +0700

    [Adityo S. Nento] - Mobile 375px

--- src/components/Body/LastestProject.js
+++ src/components/Body/LastestProject.js
@@ -11,8 +11,8 @@ class LastestProject extends Component {
           <div className="project__title">
             <h1>MY LASTEST PROJECT</h1>
           </div>
-          <div className="project__url">
-            <ReactPlayer id="video" url="https://www.youtube.com/watch?v=QGMi1DgFd_A"/>
+          <div >
+            <ReactPlayer url="https://www.youtube.com/watch?v=QGMi1DgFd_A" width="100%" height="100%" style={{padding:"0px 10px", position: "relative"}}  />
           </div>
           </div>
         </div>
